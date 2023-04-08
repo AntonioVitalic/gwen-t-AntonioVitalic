14a8f39b17184a0571a382d9787ef69e67da5cef
AntonioVitalic <48534043+AntonioVitalic@users.noreply.github.com>
2023-04-08 01:22:29 -0400

    Se corrige el path de carpeta Test. Se crea trait AccionesJugador que se "extenderá" en class Jugador y class Computadora, se crea trait Carta con la intención de usarlo en class CartaUnidad y class CartaClima.

--- src/main/scala/gwent/main.scala
+++ src/main/scala/gwent/main.scala
@@ -6,14 +6,37 @@ trait Jugador(val Nombre: String, var SeccionTablero: String,
               var ContadorGemas: int, var MazoCartas: List[Carta],
               var ManoCartas: List[Carta]) {
 }
-class Usuario extends Jugador {
-  def this(Nombre: String, SeccionTablero: String, ContadorGemas: int, MazoCartas: List[Carta], ManoCartas: List[Carta]) {
+
+trait AccionesJugador {
+  def JugarCartaMano: Unit
+
+  def RobarCartaMazo: Unit
+}
+
+class Usuario extends Jugador with AccionesJugador{
+  def this(Nombre: String, SeccionTablero: String, ContadorGemas: int,
+           MazoCartas: List[Carta], ManoCartas: List[Carta]): Any : Unit = {
     this(Nombre, SeccionTablero, ContadorGemas, MazoCartas, ManoCartas)
   }
 }
 
-class Computadora extends Jugador {
-  def this(Nombre: String, SeccionTablero: String, ContadorGemas: int, MazoCartas: List[Carta], ManoCartas: List[Carta]) {
+class Computadora extends Jugador with AccionesJugador{
+  def this(Nombre: String, SeccionTablero: String, ContadorGemas: int,
+           MazoCartas: List[Carta], ManoCartas: List[Carta]) {
     this(Nombre, SeccionTablero, ContadorGemas, MazoCartas, ManoCartas)
   }
+}
+
+// Para la entrega 1, solo se pide la seccion 1.1 Jugador y 1.3 Cartas, por lo que
+// no se implementarán los efectos de las cartas
+trait Carta(var Clasificacion: String){
+
+}
+
+class CartaUnidad extends Carta{
+
+}
+
+class CartaClima extends Carta{
+
 }
