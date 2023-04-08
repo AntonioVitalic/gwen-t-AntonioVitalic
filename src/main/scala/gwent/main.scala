8f630a02c97d5f3bd7746f869f7ec4113d0a67e8
AntonioVitalic <48534043+AntonioVitalic@users.noreply.github.com>
2023-04-08 00:21:32 -0400

    Crea trait Jugador, el cual se extiende en las clases Usuario y Computadora

--- src/main/scala/gwent/main.scala
+++ src/main/scala/gwent/main.scala
@@ -1,7 +1,19 @@
 package cl.uchile.dcc
 package gwent
 
-@main
-def main(): Unit = {
-  println("Hello world!")
+// trait Jugador -> class Usuario y Computadora
+trait Jugador(val Nombre: String, var SeccionTablero: String,
+              var ContadorGemas: int, var MazoCartas: List[Carta],
+              var ManoCartas: List[Carta]) {
+}
+class Usuario extends Jugador {
+  def this(Nombre: String, SeccionTablero: String, ContadorGemas: int, MazoCartas: List[Carta], ManoCartas: List[Carta]) {
+    this(Nombre, SeccionTablero, ContadorGemas, MazoCartas, ManoCartas)
+  }
+}
+
+class Computadora extends Jugador {
+  def this(Nombre: String, SeccionTablero: String, ContadorGemas: int, MazoCartas: List[Carta], ManoCartas: List[Carta]) {
+    this(Nombre, SeccionTablero, ContadorGemas, MazoCartas, ManoCartas)
+  }
 }
